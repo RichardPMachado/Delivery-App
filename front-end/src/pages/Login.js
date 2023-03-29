b74f77d497f01c09ea9ecc12a31c301b0b2666d4
Lizia Towata <liziatowata@gmail.com>
2023-03-29 00:27:10 -0300

    feat[login front-end]: criação da tela de login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,7 +1,74 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = ({ target }) => {
+    setEmail(target.value);
+  };
+
+  const handlePassword = ({ target }) => {
+    setPassword(target.value);
+  };
+
+  const formeValidate = (() => {
+    const minPassword = 6;
+    const regex = /\S+@\S+\.\S+/;
+    const validEmail = regex.test(email);
+    return !!(!validEmail
+      || password.length < minPassword
+      || password === ''
+      || email === '');
+  });
+
   return (
     <div>
-      <h1>Login</h1>
+      <h1> Login </h1>
+      <label htmlFor="email">
+        Email
+        <input
+          data-testid="common_login__input-email"
+          type="email"
+          id="email"
+          className="email"
+          placeholder="email@email.com"
+          value={ email }
+          onChange={ handleEmail }
+        />
+      </label>
+
+      <label htmlFor="password">
+        Senha
+        <input
+          data-testid="common_login__input-password"
+          type="password"
+          id="password"
+          className="password"
+          placeholder="password"
+          value={ password }
+          onChange={ handlePassword }
+        />
+      </label>
+
+      <button
+        data-testid="login_common__button"
+        type="button"
+        onClick={ () => {} }
+        disabled={ formeValidate() }
+      >
+        Login
+      </button>
+
+      <button
+        data-testid="common_login__button-register"
+        type="button"
+      >
+        <Link to="/register">
+          Cadastrar
+        </Link>
+      </button>
     </div>
   );
 }
