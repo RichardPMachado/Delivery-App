fca428dbbeffcf81ebbb92e656f9262ecb8b6e8e
EuGuiXtd <guilhermedpjf@gmail.com>
2023-03-29 01:53:19 -0300

    feat[login validations backend]: validações do login no backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const user = require('../database/controller/User');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
